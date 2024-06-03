7f00ea5c9054da6e14d6872802c6e46021f5bdfc
Umang <umang.multiqos@gmail.com>
2024-06-03 10:52:50 +0530

    Initial Commit

---
+++ MoodTrackingSwiftUI/MoodTrackingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackingSwiftUIApp.swift
+//  MoodTrackingSwiftUI
+//
+//  Created by MQF-6 on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
